51dc370ef18f9983e51e85c07785a533fceb303c
supernoir <kawohl.bentodesign@gmail.com>
2017-12-01 08:28:40 +0100

    emits message from client and server

--- index.js
+++ index.js
@@ -9,8 +9,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', function(socket) {
   console.log('New Connection established');
-});
 
+  socket.emit('message-from-server', {
+    greeting: 'Hello from Server',
+  });
+
+  socket.on('message-from-client', function(msg) {
+    console.log(msg);
+  });
+});
 server.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
